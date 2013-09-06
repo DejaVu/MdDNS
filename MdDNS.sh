1d70e18a93fcf97476924ff5822ba9eff80848e9
DejaVu <leonsmy@googlemail.com>
2013-09-06 18:07:11 +0100

    Update MdDNS.sh

--- MdDNS.sh
+++ MdDNS.sh
@@ -8,7 +8,7 @@ do
 
 IP=`wget -O - -q icanhazip.com`
 
-echo -e "dDNS Script - By DejaVu"
+echo -e "MdDNS Script - By DejaVu"
 echo -e "github.com/DejaVu/MdDNS"
 echo -e "\e[00;30m============================\e[00m"
 echo -e "Current IP is \e[00;34m$IP\e[00m"
